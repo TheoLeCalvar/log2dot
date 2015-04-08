0c489551bbc8beb53f1168874e8ff6f90688b380
Théo Le Calvar <theo.lecalvar@gmail.com>
2015-04-08 21:07:00 +0200

    Plus de vérifications sur la conversion

--- src/toDot.cpp
+++ src/toDot.cpp
@@ -51,22 +51,23 @@ void save_to_dot(const string & path, vector<regle*> & faits, vector<regle*> reg
             if (a1) {
                 string termes = a1->termes_to_string();
 
-                for (atome *b: *a1->termes) {
-                    bool          alone = true;
-                    ostringstream os2;
-                    os2 << "r" << cluster_count << "_" << a1->predicat << "_" << b->id() << "(" << termes << ")";
-                    string p_droite = regex_replace(os2.str(), quotes, "\\$&");
+                if (a1->termes)
+                    for (atome *b: *a1->termes) {
+                        bool          alone = true;
+                        ostringstream os2;
+                        os2 << "r" << cluster_count << "_" << a1->predicat << "_" << b->id() << "(" << termes << ")";
+                        string p_droite = regex_replace(os2.str(), quotes, "\\$&");
 
-                    for (pair<string, string> p: p_gauche) {
-                        if (b->id() == p.second) {
-                            of << "\t\t\"" << p_droite << "\"" << " -> " << "\"" << p.first << "\"" << (a->negatif ? " [style=dashed] " : "") << ";\n";
-                            alone = false;
+                        for (pair<string, string> p: p_gauche) {
+                            if (b->id() == p.second) {
+                                of << "\t\t\"" << p_droite << "\"" << " -> " << "\"" << p.first << "\"" << (a->negatif ? " [style=dashed] " : "") << ";\n";
+                                alone = false;
+                            }
                         }
-                    }
 
-                    if(alone)
-                        of << "\t\t\"" << p_droite << "\";\n";
-                }
+                        if(alone)
+                            of << "\t\t\"" << p_droite << "\";\n";
+                    }
             }
         }
 
@@ -80,7 +81,8 @@ void save_to_dot(const string & path, vector<regle*> & faits, vector<regle*> reg
     //traitement des contraintes
     for (regle* r: contraintes) {
         of <<   "\tsubgraph cluster_" << cluster_count << "{\n"
-                "\t\tlabel=\"Contrainte " << cluster_count << "\";\n";
+                "\t\tlabel=\"Contrainte " << cluster_count << "\";\n"
+                "\t\tr_" << cluster_count << "_contraintes_⊥ [label=⊥];\n";
 
             for (atome* a: *r->corps) {
                 atome_predicat * a1 = nullptr;
@@ -88,12 +90,15 @@ void save_to_dot(const string & path, vector<regle*> & faits, vector<regle*> reg
 
                     string termes = a1->termes_to_string();
 
-                    for (atome* b: *a1->termes) {
-                        ostringstream os;
-                        os << "r" << cluster_count << "_" << a1->predicat << "_" << b->id() << "(" << termes << ")";
+                    if (a1->termes)
+                        for (atome* b: *a1->termes) {
+                            ostringstream os;
+                            if (dynamic_cast<atome_var*>(b)) {
+                                os << "r" << cluster_count << "_" << a1->predicat << "_" << b->id() << "(" << termes << ")";
 
-                        of << "\t\t\"" << regex_replace(os.str(), quotes, "\\$&") << "\"-> ⊥;\n";
-                    }
+                                of << "\t\t\"" << regex_replace(os.str(), quotes, "\\$&") << "\"-> r_" <<cluster_count << "_contraintes_⊥;\n";
+                            }
+                        }
 
                 }
 
